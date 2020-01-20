5a6f534a7710dd1faba32a494f53f91c898fdba9
Yao <yekongbuye@gmail.com>
2020-01-20 22:22:45 +0800

    fix: 测试npm发布

--- src/emoji/parseUnicode.ts
+++ src/emoji/parseUnicode.ts
@@ -3,6 +3,7 @@
  * @see "\\u00253A" => "\u00253A"
  * @author 原始地址：https://stackoverflow.com/questions/33685680/emoji-surrogate-string-with-javascript-how-to-parse
  * @param str
+ *
  */
 export function parseUnicode(str: string) {
   let r = /\\u([\d\w]{4})/gi
